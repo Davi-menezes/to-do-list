7a6637e60d7228607388acf6acf6ff80beeb31fc
Davi Menezes de Oliveira <74253285+Davi-menezes@users.noreply.github.com>
2025-03-12 21:03:53 -0300

    Delete todolist directory

--- todolist/backend/todolist.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE todo_list_db;
-USE todo_list_db;
-CREATE TABLE tasks (
-id INT AUTO_INCREMENT PRIMARY KEY,
-task VARCHAR(255) NOT NULL,
-status ENUM('pendente', 'concluida') DEFAULT 'pendente',
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
